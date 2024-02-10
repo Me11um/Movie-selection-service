66e94c1c1fb5424f0c10f01a1176860a02ffc6ba
PGS1337 <dorov567@gmail.com>
2024-02-10 22:42:26 +0500

    v1.2 (Beta)

--- test_project/main/movies/getmovies.py
+++ test_project/main/movies/getmovies.py
@@ -1,26 +1,60 @@
 from bs4 import BeautifulSoup
 import requests
+import random
 
-headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36'
-      }
+def getmoviesname(genre):
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36'
+          }
 
-url = 'https://www.kinoafisha.info/rating/movies/'
-page = requests.get(url, headers = headers)
-print(page.status_code)
+    url = 'https://www.kinoafisha.info/rating/movies/' + genre + '/'
+    if genre == 'default':
+        url = 'https://www.kinoafisha.info/rating/movies/'
+    page = requests.get(url, headers = headers)
 
-movies = []
-filteredmovies = []
-filteredimages = []
+    movies = []
 
-soup = BeautifulSoup(page.text, "html.parser")
-movies = soup.findAll('a' , class_='movieItem_title')
-images = soup.findAll('img' , class_='picture_image')
+    filteredmovies = []
+    filteredimages = []
+    filteredgenre = []
+    filteredyear = []
+    filteredrating = []
+    filteredhref = []
 
-for data in movies:
-    filteredmovies.append(data.text)
+    soup = BeautifulSoup(page.text, "html.parser")
+    movies = soup.findAll('a' , class_='movieItem_title')
+    images = soup.findAll('img' , class_='picture_image')
+    moviesgenre = soup.findAll('span', class_='movieItem_genres')
+    moviesyear = soup.findAll('span', class_='movieItem_year')
+    moviesrating = soup.findAll('span', class_='miniRating')
 
-for data in images:
-    filteredimages.append(data['data-picture'])
+    nums = []
 
-#print(filteredimages)
+    for i in range(3):
+        if i == 0:
+            nums.append(random.randint(0, len(movies) - 1))
+        else:
+            nums.append(random.randint(0, len(movies) - 1))
+            while nums[i] == nums[i - 1]:
+                nums[i] = random.randint(0, len(movies) - 1)
+
+    for i in nums:
+        filteredmovies.append(movies[i].text)
+        filteredimages.append(images[i]['data-picture'])
+        filteredgenre.append(moviesgenre[i].text)
+        filteredyear.append(moviesyear[i].text)
+        filteredrating.append("рейтинг: " + moviesrating[i].text)
+        filteredhref.append(movies[i]['href'])
+
+
+    rdata = {
+        'movies': filteredmovies,
+        'images': filteredimages,
+        'genre': filteredgenre,
+        'year': filteredyear,
+        'rating': filteredrating,
+        'hrefs': filteredhref
+    }
+
+    return rdata
+    #print(filteredimages)
--- test_project/main/views.py
+++ test_project/main/views.py
@@ -4,11 +4,24 @@ from .movies.getmovies import *
 # Create your views here.
 def index(request):
     data = {
-        'title': 'Сервис для подброки фильмов',
-        'movies': filteredmovies,
-        'images': filteredimages
+        'title': 'Сервис для подборки фильмов'
     }
     return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
+
+def app(request):
+    if request.method == "POST":
+        data = getmoviesname(request.POST.get("genre"))
+        return render(request, 'main/app.html', data)
+    else:
+        data = {
+            'movies': [],  # filteredmovies,
+            'images': [],  # filteredimages
+            'genre': [],
+            'year': [],
+            'rating': [],
+            'hrefs': []
+        }
+        return render(request, 'main/app.html', data)
